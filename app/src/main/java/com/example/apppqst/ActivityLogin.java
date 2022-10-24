0fc6a08a8b0aa54c1c36367b7da74a72d96b20a5
hatien <hatiennguyen39@gmail.com>
2022-10-24 23:38:15 +0700

    Layout1,2

---
+++ app/src/main/java/com/example/apppqst/ActivityLogin.java
@@ -0,0 +1,47 @@
+package com.example.apppqst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ActivityLogin extends AppCompatActivity {
+    Button loginbutton;
+    ImageView backbtn;
+    EditText password, username;
+    TextView forgot;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginbutton=findViewById(R.id.btn_login);
+        backbtn=findViewById(R.id.backbtn);
+        password=findViewById(R.id.inputpass);
+        username=findViewById(R.id.username);
+        forgot=findViewById(R.id.forgotpass);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(view.getContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String LoginUsername = String.valueOf(username.getText());
+                String Password = String.valueOf(password.getText());
+                Intent intent=new Intent(view.getContext(),Bluetooth.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/apppqst/MainActivity.java
+++ app/src/main/java/com/example/apppqst/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.apppqst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginbutton,registerbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginbutton=findViewById(R.id.btn_login);
+        registerbutton=findViewById(R.id.btn_register);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(view.getContext(),ActivityLogin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
